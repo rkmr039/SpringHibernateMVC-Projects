c157cf8ff131f2e619a092d04b3846679f1d908a
rkmr039 <rkmr039@gmail.com>
2019-09-18 17:45:33 +0530

    Data from MySql Table to Java to Xml File Completed

--- XML/XmlParserDemo/src/com/hcl/parserbase/Employee.java
+++ XML/XmlParserDemo/src/com/hcl/parserbase/Employee.java
@@ -2,8 +2,11 @@ package com.hcl.parserbase;
 
 import java.sql.Date;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 
+@XmlRootElement
 public class Employee {
 	
 
@@ -13,7 +16,7 @@ public class Employee {
 	private char band;
 	private Date dateOfJoin;
 	
-	
+	@XmlElement
 	public int getEmpNo() {
 		return empNo;
 	}
@@ -21,28 +24,49 @@ public class Employee {
 	public void setEmpNo(int empNo) {
 		this.empNo = empNo;
 	}
+	@XmlElement
 	public String getEmpName() {
 		return empName;
 	}
 	public void setEmpName(String empName) {
 		this.empName = empName;
 	}
+	
+	@XmlElement
 	public float getSalary() {
 		return salary;
 	}
 	public void setSalary(float salary) {
 		this.salary = salary;
 	}
+	
+	@XmlElement
 	public char getBand() {
 		return band;
 	}
 	public void setBand(char band) {
 		this.band = band;
 	}
+	
+	@XmlElement
 	public Date getDateOfJoin() {
 		return dateOfJoin;
 	}
 	public void setDateOfJoin(Date dateOfJoin) {
+		this.dateOfJoin = dateOfJoin;
+	}
+	
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(int empNo, String empName, float salary, char band, Date dateOfJoin) {
+		super();
+		this.empNo = empNo;
+		this.empName = empName;
+		this.salary = salary;
+		this.band = band;
 		this.dateOfJoin = dateOfJoin;
 	}
 
---
+++ XML/XmlParserDemo/src/com/hcl/parserbase/Employees.java
@@ -0,0 +1,22 @@
+package com.hcl.parserbase;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Employees {
+
+	private List<Employee> employees;
+
+	@XmlElement
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+}
